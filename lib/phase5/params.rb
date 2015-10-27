ff2adaf972eb0238ca621f3aebabdf6297fe233c
Cody Pizzaia <cpizzaia@asu.edu>
2015-10-27 18:02:46 -0400

    on nested params

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -1,4 +1,5 @@
 require 'uri'
+require 'byebug'
 
 module Phase5
   class Params
@@ -10,9 +11,15 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+      @req = req
+      @params = {}
+      @params.merge!(parse_www_encoded_form(req.query_string)) if req.query_string
+      @params.merge!(parse_www_encoded_form(req.body)) if req.body
+      @params.merge!(route_params)
     end
 
     def [](key)
+      @params[key.to_s]
     end
 
     # this will be useful if we want to `puts params` in the server log
@@ -29,11 +36,46 @@ module Phase5
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      params = {}
+
+      key_values = URI.decode_www_form(www_encoded_form)
+      key_values.each do |full_key, value|
+        scope = params
+        key_seq = parse_key(full_key)
+        key_seq.each_with_index do |key, idx|
+          if (idx + 1) == key_seq.count
+            scope[key] = value
+          else
+            scope[key] ||= {}
+            scope = scope[key]
+          end
+        end
+      end
+
+      params
     end
 
     # this should return an array
-    # user[address][street] should return ['user', 'address', 'street']
+    # "user[address][street]" should return ['user', 'address', 'street']
     def parse_key(key)
+      result = []
+      last_found_position = 0
+      for i in 0..key.length-1
+        if key[i] == "["
+          string = key[last_found_position..i-1]
+          string = string[0..-2] if string[-1] == "]"
+          result << string
+          last_found_position = i+1
+        elsif i == key.length-1
+          string = key[last_found_position..i]
+          if string[-1] == "]"
+            result << string[0..-2]
+          else
+            result << string
+          end
+        end
+      end
+      result
     end
   end
 end
